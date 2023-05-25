31b86959b76f430ef640b47c18ad1f77484307e3
hamza <hamzabousseta1@gmail.com>
2023-05-25 16:49:54 -0700

    hot fix 3

--- main.c
+++ main.c
@@ -5,41 +5,40 @@
 
 void execute_command(char *command)
 {
-    printf("Executing command: %s\n", command);
-    /* You can replace this with the actual execution logic
-     * Here, we simply print the command that would have been executed
-     */
+	/* Print a message indicating the command to be executed */
+	printf("Executing command: %s\n", command);
+	/* You can replace this with the actual execution logic
+	 * Here, we simply print the command that would have been executed
+	 */
 }
 
 int main(void)
 {
-    while (1)
-    {
-        /* Display the prompt */
-        printf("$ ");
-        fflush(stdout);
+	while (1)
+	{
+		/* Display the prompt */
+		printf("$ ");
+		fflush(stdout);
 
-        /* Read the command from the user */
-        char command[MAX_COMMAND_LENGTH];
-        if (fgets(command, sizeof(command), stdin) == NULL)
-        {
-            /* Handle end of file condition (Ctrl+D) */
-            printf("\n");
-            break;
-        }
+		/* Read the command from the user */
+		char command[MAX_COMMAND_LENGTH];
+		if (fgets(command, sizeof(command), stdin) == NULL)
+		{
+			/* Handle end of file condition (Ctrl+D) */
+			printf("\n");
+			break;
+		}
 
-        /* Remove the trailing newline character */
-        command[strcspn(command, "\n")] = '\0';
+		/* Remove the trailing newline character */
+		command[strcspn(command, "\n")] = '\0';
 
-        /* Check if the user wants to exit */
-        if (strcmp(command, "exit") == 0)
-        {
-            break;
-        }
+		/* Check if the user wants to exit */
+		if (strcmp(command, "exit") == 0)
+			break;
 
-        /* Execute the command */
-        execute_command(command);
-    }
+		/* Execute the command */
+		execute_command(command);
+	}
 
-    return 0;
+	return (0);
 }
