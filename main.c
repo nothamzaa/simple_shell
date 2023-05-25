03361896342ec6a879b17e2a4a12d400ea561a3a
hamza <hamzabousseta1@gmail.com>
2023-05-25 16:22:01 -0700

    lets start again

--- main.c
+++ main.c
@@ -1,30 +1,94 @@
-#include "minishell.h"
+#include "main.h"
+
 
 /**
- * main - entry point
+ * _strdup - Duplicates a string
+ * @str: The string to duplicate
  *
- * @argc: number of arguments given to the program
- * @argv: arguments list
- *
- * Return: returns the value of the last executed command
+ * Return: The pointer to the duplicated string
  */
-int main(int argc, char **argv)
+char *_strdup(const char *str)
 {
-	char *cmdline = NULL;
-	char prompt[] = "(hsh) ";
-	Bool status = true;
+	char *dup;
+	int length = 0, i;
 
-	(void)argc; /* Silence unused parameter warning */
-	name = (argc > 0) ? argv[0] : NULL;
+	if (str == NULL)
+		return (NULL);
 
-	while (status)
+	while (str[length])
+		length++;
+
+	dup = malloc(sizeof(char) * (length + 1));
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; i <= length; i++)
+		dup[i] = str[i];
+
+	return (dup);
+}
+
+/**
+ * print_array - Prints an array of integers
+ * @array: The array to print
+ * @size: The size of the array
+ */
+void print_array(const int *array, size_t size)
+{
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return;
+
+	for (i = 0; i < size; i++)
 	{
-		write(STDOUT_FILENO, prompt, strlen(prompt));
-		cmdline = get_user_input();
-		/* Process the command */
+		printf("%d", array[i]);
+		if (i < size - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
+
+/**
+ * swap_int - Swaps the values of two integers
+ * @a: The first integer
+ * @b: The second integer
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	if (a == NULL || b == NULL)
+		return;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int array[] = {5, 2, 4, 3, 1};
+	size_t size = sizeof(array) / sizeof(array[0]);
+	int i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap_int(&array[j], &array[j + 1]);
+			}
+		}
 	}
 
-	free(cmdline); /* Remember to free dynamically allocated memory */
+	print_array(array, size);
 
-	return 0;
+	return (0);
 }
---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef EXAMPLEHEADER_H
+#define EXAMPLEHEADER_H
+
+/* Function prototypes */
+char *_strdup(const char *str);
+void print_array(const int *array, size_t size);
+void swap_int(int *a, int *b);
+
+#endif /* EXAMPLEHEADER_H */
